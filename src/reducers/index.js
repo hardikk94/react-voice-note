d2005337199d97e695e91f02eb8542fa672e5c0f
hardikk94 <hardikkothari46@gmail.com>
2021-08-27 13:31:27 +0530

    Create a player and voice note reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,11 @@
-   
+import { combineReducers } from "redux";
+import voiceNotes from './voicenote-reducer';
+import player from './player-reducer';
+
+
+const reducer = combineReducers({
+    voiceNotes,
+    player,
+});
+
+export default reducer;
---
+++ src/reducers/voicenote-reducer.js
@@ -0,0 +1,73 @@
+import * as type from '../constant';
+const initialState = {
+    success: false,
+    isLoading: false,    
+    voiceNoteList: [],
+    error:null,    
+};
+
+const VoiceNoteList = (state = initialState,action) => {
+    switch(action.type) {
+        // voice list
+        case type.VOICE_LIST_REQUESTED: {
+            return {
+                ...state,     
+                success:false,           
+                isLoading: true,                                
+            };
+        }
+        case type.VOICE_LIST_COMPLETED: {
+            return {
+                ...state,     
+                success:true,           
+                isLoading: false,   
+                voiceList:action.payload.voiceNoteList                             
+            };
+        }
+
+        case type.VOICE_LIST_ERROR: {
+            return {
+                ...state,      
+                success:true,                     
+                isLoading: false,  
+                error:action.payload.error                              
+            };
+        }
+
+        // add voice list
+        case type.VOICE_ADD_REQUESTED: {
+            return {
+                ...state,    
+                success:false,                       
+                isLoading: true,                                
+            };
+        }
+        case type.VOICE_ADD_COMPLETED: {
+            return {
+                ...state,   
+                success:true,                        
+                isLoading: false,                                            
+            };
+        }
+
+          // delete voice list
+        case type.VOICE_DELETE_REQUESTED: {
+            return {
+                ...state,    
+                success:false,                       
+                isLoading: true,                                
+            };
+        }
+        case type.VOICE_DELETE_COMPLETED: {
+            return {
+                ...state,   
+                success:true,                        
+                isLoading: false,                                            
+            };
+        }
+        default:
+            return state;
+    }
+}
+
+export default VoiceNoteList
