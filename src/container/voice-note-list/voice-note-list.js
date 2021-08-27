ade1fc5aba4a244e378efc6a2938007c92871fce
hardikk94 <hardikkothari46@gmail.com>
2021-08-27 17:24:59 +0530

    Handle list No records found

--- src/container/voice-note-list/voice-note-list.js
+++ src/container/voice-note-list/voice-note-list.js
@@ -3,16 +3,23 @@ import { useEffect } from 'react';
 import './voice-note-list.scss';
 import VoiceNoteView from './../../component/voiceNoteView/voice-note-view';
 import { voiceNoteListRequest } from '../../actions/voice-note-action'
-
+import { useHistory } from "react-router-dom";
 const VoiceNotesList = () => {
+    const history = useHistory();
     const dispatch = useDispatch();
     const list = useSelector((state) => state.voiceNotes.voiceNoteList)
+    const isLoading = useSelector((state) => state.voiceNotes.isLoading)
     useEffect(() => {
         voiceNoteListRequest(dispatch)
     },[])
+
+    const backToRecorder = () =>{
+        history.push("/voice-recorder")
+    }
+
     return (
         <div className="voice_list_wrapper">
-            {
+            { list && list.length > 0 ? (
                 list.map((notes) => {
                     return (
                         <VoiceNoteView                        
@@ -21,7 +28,12 @@ const VoiceNotesList = () => {
                       />
                     )
                 })
-            }
+            ) : (
+                <div className="no-list">
+                    <span>No Voice Notes here...!! <input type="button" value="Back" id="back" name="back" className="btn_back" onClick={backToRecorder} /></span>
+                </div>
+            )                
+        }
         </div>
     )
 }
