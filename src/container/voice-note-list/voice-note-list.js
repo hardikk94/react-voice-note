1117c3f8275eedeec82fc52a443bc3b9ce58db27
hardikk94 <hardikkothari46@gmail.com>
2021-08-27 17:44:57 +0530

    Show loading on listing of voice notes

--- src/container/voice-note-list/voice-note-list.js
+++ src/container/voice-note-list/voice-note-list.js
@@ -1,9 +1,10 @@
-import {useSelector,useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import './voice-note-list.scss';
 import VoiceNoteView from './../../component/voiceNoteView/voice-note-view';
 import { voiceNoteListRequest } from '../../actions/voice-note-action'
 import { useHistory } from "react-router-dom";
+import BeatLoader from "react-spinners/BeatLoader";
 const VoiceNotesList = () => {
     const history = useHistory();
     const dispatch = useDispatch();
@@ -11,29 +12,41 @@ const VoiceNotesList = () => {
     const isLoading = useSelector((state) => state.voiceNotes.isLoading)
     useEffect(() => {
         voiceNoteListRequest(dispatch)
-    },[])
+    }, [])
 
-    const backToRecorder = () =>{
+    const backToRecorder = () => {
         history.push("/voice-recorder")
     }
 
     return (
         <div className="voice_list_wrapper">
-            { list && list.length > 0 ? (
+            {
+                isLoading ? (
+                    <div className="loader">
+                        <BeatLoader color={'#ffc107'} loading={isLoading} size={15} />
+                    </div>
+                ) : null
+            }
+
+            {list && list.length > 0 && !isLoading ? (
                 list.map((notes) => {
                     return (
-                        <VoiceNoteView                        
+                        <VoiceNoteView
                             notes={notes}
                             key={notes.id}
-                      />
+                        />
                     )
                 })
             ) : (
                 <div className="no-list">
-                    <span>No Voice Notes here...!! <input type="button" value="Back" id="back" name="back" className="btn_back" onClick={backToRecorder} /></span>
+                    {
+                        !isLoading ? (
+                            <span>No Voice Notes here...!! <input type="button" value="Back" id="back" name="back" className="btn_back" onClick={backToRecorder} /></span>
+                        ) : null
+                    }
                 </div>
-            )                
-        }
+            )
+            }
         </div>
     )
 }
